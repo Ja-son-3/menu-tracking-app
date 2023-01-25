88bdca45f6977d5cc769c2760513c67bee090cd8
Jason Huang <jason97521@gmail.com>
2023-01-25 03:32:21 -0500

    routes, controllers, css, schemas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-//add controllers
-//const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const indexController = require('../controllers/index') 
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', indexController.getHome)
-router.
+router.post('/q', indexController.getSearch)
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
-const indexRouter = require('./routes/index')
-const menuRouter = require('./routes/menu')
-const loginRouter = require('./routes/login')
-
+const indexRouter = require('./routes/index');
+const menuRouter = require('./routes/menu');
+const loginRouter = require('./routes/login');
 
 require('dotenv').config({path: './config/.env'})
 
@@ -28,23 +27,23 @@ app.use(logger('dev'))
 app.use(
     session({
       secret: 'keyboard cat',
+      cookie:{maxAge:60000},
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: new MongoStore({ mongoUrl:process.env.DB_STRING }),
     })
   )
   
 // Passport middleware
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(flash())
-
-app.use('/', indexRouter)
-app.use('/menu', menuRouter)
-app.use('/login', loginRouter)
-
+  
+app.use('/', indexRouter);
+app.use('/menu', menuRouter);
+app.use('/login', loginRouter);
  
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on ${process.env.DB_STRING}`)
+    console.log('Server is running, you better catch it!')
 })    
